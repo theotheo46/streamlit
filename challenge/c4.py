70d8cbff14109a2628eb122959704992f38c1c2b
theo <theotheo46@gmail.com>
2025-01-20 18:30:10 +0300

    diploma

--- challenge/c4.py
+++ challenge/c4.py
@@ -6,4 +6,5 @@ dataframe = pd.DataFrame(
     np.random.randn(10, 20),
     columns=('col %d' % i for i in range(20)))
 
-st.dataframe(dataframe.style.highlight_max(axis=0))
+# st.dataframe(dataframe.style.highlight_max(axis=0))
+st.dataframe(dataframe.style.highlight_quantile(axis=0, q_left=0.5))
