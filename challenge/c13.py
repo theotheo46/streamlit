1e38bc30360f11fa97e42a8bd4c4cab5bf60bedb
theo <theotheo46@gmail.com>
2025-01-21 12:29:21 +0300

    test

---
+++ challenge/c13.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+
+@st.cache_data
+def load_data(url):
+    df = pd.read_csv(url)  # 👈 Download the data
+    return df
+
+df = load_data("https://github.com/plotly/datasets/raw/master/uber-rides-data1.csv")
+st.dataframe(df)
+
+st.button("Rerun")
